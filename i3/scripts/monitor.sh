cc5f143d1237f4052e401d4e5dfadeb12148a8e3
TheNofis <matbey122@gmail.com>
2025-06-20 21:11:13 +0000

    add compositor

--- i3/scripts/monitor.sh
+++ i3/scripts/monitor.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 #MONITOR SETTINGS
-xrandr --output HDMI-0 --mode 1920x1080 --pos -1920x0 \
---output DP-4 --mode 1920x1080 --rate 180 --pos 0x0 \
---output DVI-D-0 --mode 1920x1080 --pos 1920x-215 --rotate right
+xrandr --output HDMI-0 --mode 1920x1080 --left-of DP-0 \
+  --output DP-0 --primary --mode 1920x1080 --rate 180 \
+  --output DVI-D-0 --mode 1920x1080 --right-of DP-0 --rotate right
 # xrandr --output DP-4 --mode 1920x1080 --rate 180 --pos 0x0 \
 
 # SET WALLPAPER
 # feh --bg-fill ~/Wallpaper/1.jpg
 # feh --bg-fill ~/Wallpaper/6.jpg ~/Wallpaper/2.jpg ~/Wallpaper/2.jpg
 feh --randomize --bg-fill ~/Wallpaper/*.jpg
+xcompmgr -c &
